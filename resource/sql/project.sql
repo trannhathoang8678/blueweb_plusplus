82c121e9612945fa7be20a933c64c578f6b07561
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-09-15 22:38:44 +0700

    update sql

--- resource/sql/project.sql
+++ resource/sql/project.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -28,33 +28,14 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`MAKER`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`MAKER` (
-  `maker_id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `year_create` INT NULL,
-  `place_create` VARCHAR(45) NULL,
-  PRIMARY KEY (`maker_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
 -- Table `mydb`.`TYPE`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`TYPE` (
   `type_id` INT NOT NULL AUTO_INCREMENT,
-  `maker_id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
+  `maker` VARCHAR(45) NOT NULL,
   `product_line` VARCHAR(45) NULL,
-  PRIMARY KEY (`type_id`),
-  INDEX `fk_TYPE_MAKER1_idx` (`maker_id` ASC) VISIBLE,
-  CONSTRAINT `fk_TYPE_MAKER1`
-    FOREIGN KEY (`maker_id`)
-    REFERENCES `mydb`.`MAKER` (`maker_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`type_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
@@ -63,13 +44,13 @@ DEFAULT CHARACTER SET = utf8mb4;
 -- Table `mydb`.`DISCOUNT`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`DISCOUNT` (
-  `discount_id` INT NOT NULL,
-  `start_time` TIMESTAMP(6) NOT NULL,
-  `end_time` TIMESTAMP(6) NULL,
-  `percent` INT(100) NOT NULL,
+  `discount_id` INT NOT NULL AUTO_INCREMENT,
+  `start_time` TIMESTAMP NULL,
+  `end_time` TIMESTAMP NULL,
+  `percent` INT(7) NOT NULL,
   PRIMARY KEY (`discount_id`))
 ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
+DEFAULT CHARACTER SET = eucjpms;
 
 
 -- -----------------------------------------------------
@@ -77,17 +58,19 @@ DEFAULT CHARACTER SET = utf8mb4;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`PRODUCT` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
-  `discount_id` INT NOT NULL,
-  `type_id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  `price` DECIMAL(16,3) NOT NULL,
+  `type_id` INT NOT NULL,
   `specification` TEXT NULL,
   `url_image` VARCHAR(45) NULL,
   `number` VARCHAR(45) NULL,
+  `year_create` INT NULL,
+  `place_create` VARCHAR(45) NULL,
+  `price` DECIMAL(16,3) NOT NULL,
   `instalment` TINYINT(1) NULL,
+  `discount_id` INT NOT NULL,
   PRIMARY KEY (`product_id`),
-  INDEX `fk_PRODUCT_TYPE1_idx` (`type_id` ASC) VISIBLE,
-  INDEX `fk_PRODUCT_DISCOUNT1_idx` (`discount_id` ASC) VISIBLE,
+  INDEX `fk_PRODUCT_TYPE1_idx` (`type_id` ASC),
+  INDEX `fk_PRODUCT_DISCOUNT1_idx` (`discount_id` ASC),
   CONSTRAINT `fk_PRODUCT_TYPE1`
     FOREIGN KEY (`type_id`)
     REFERENCES `mydb`.`TYPE` (`type_id`)
@@ -109,10 +92,11 @@ CREATE TABLE IF NOT EXISTS `mydb`.`IMPORT` (
   `product_id` INT NOT NULL,
   `provider_id` INT NOT NULL,
   `number` INT NULL,
-  `note` VARCHAR(45) NULL,
   `price` DECIMAL NOT NULL,
-  INDEX `fk_PROVIDE_PRODUCT_idx` (`product_id` ASC) VISIBLE,
-  INDEX `fk_PROVIDE_PROVIDER1_idx` (`provider_id` ASC) VISIBLE)
+  `note` VARCHAR(45) NULL,
+  `created_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX `fk_PROVIDE_PRODUCT_idx` (`product_id` ASC),
+  INDEX `fk_PROVIDE_PROVIDER1_idx` (`provider_id` ASC))
 ENGINE = MRG_MyISAM
 DEFAULT CHARACTER SET = utf8mb4;
 
@@ -123,6 +107,7 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `mydb`.`CUSTOMER` (
   `customer_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
+  `phone_number` CHAR(10) NULL,
   `note` VARCHAR(45) NULL,
   PRIMARY KEY (`customer_id`))
 ENGINE = InnoDB
@@ -135,12 +120,9 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `mydb`.`ORDER` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `customer_id` INT NOT NULL,
-  `payment_mode` VARCHAR(45) NULL,
-  `option` VARCHAR(45) NULL,
-  `number_product` INT NULL,
   `address` VARCHAR(45) NULL,
-  `created_time` TIMESTAMP(6) NULL DEFAULT CURRENT TIMESTAMP,
-  INDEX `fk_ORDER_CUSTOMER1_idx` (`customer_id` ASC) VISIBLE,
+  `created_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX `fk_ORDER_CUSTOMER1_idx` (`customer_id` ASC),
   PRIMARY KEY (`order_id`),
   CONSTRAINT `fk_ORDER_CUSTOMER1`
     FOREIGN KEY (`customer_id`)
@@ -157,12 +139,13 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `mydb`.`MARKETING_ARTICLE` (
   `article_id` INT NOT NULL AUTO_INCREMENT,
   `provider_id` INT NOT NULL,
+  `name` VARCHAR(75) NOT NULL,
   `url_image` VARCHAR(45) NULL,
   `content` TEXT NULL,
-  `created_time` TIMESTAMP(6) NULL DEFAULT CURRENT TIMESTAMP,
-  `update_time` TIMESTAMP(6) NULL DEFAULT CURRENT TIMESTAMP ON UPDATE CURRENT TIMESTAMP,
+  `created_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`article_id`),
-  INDEX `fk_MARKETING_ARTICLE_PROVIDER1_idx` (`provider_id` ASC) VISIBLE,
+  INDEX `fk_MARKETING_ARTICLE_PROVIDER1_idx` (`provider_id` ASC),
   CONSTRAINT `fk_MARKETING_ARTICLE_PROVIDER1`
     FOREIGN KEY (`provider_id`)
     REFERENCES `mydb`.`PROVIDER` (`provider_id`)
@@ -181,9 +164,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`REVIEW` (
   `RATE` DECIMAL(1,1) NULL,
   `url_image` VARCHAR(45) NULL,
   `content` TEXT NULL,
-  `created_time` TIMESTAMP(6) NULL DEFAULT CURRENT TIMESTAMP,
-  PRIMARY KEY (`customer_id`),
-  INDEX `fk_REVIEW_PRODUCT1_idx` (`product_id` ASC) VISIBLE,
+  `created_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX `fk_REVIEW_PRODUCT1_idx` (`product_id` ASC),
   CONSTRAINT `fk_REVIEW_CUSTOMER1`
     FOREIGN KEY (`customer_id`)
     REFERENCES `mydb`.`CUSTOMER` (`customer_id`)
@@ -204,8 +186,8 @@ DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE IF NOT EXISTS `mydb`.`ARTICLE_PRODUCT` (
   `article_id` INT NOT NULL,
   `product_id` INT NOT NULL,
-  INDEX `fk_ARTICLE_PRODUCT_MARKETING_ARTICLE1_idx` (`article_id` ASC) VISIBLE,
-  INDEX `fk_ARTICLE_PRODUCT_PRODUCT1_idx` (`product_id` ASC) VISIBLE,
+  INDEX `fk_ARTICLE_PRODUCT_MARKETING_ARTICLE1_idx` (`article_id` ASC),
+  INDEX `fk_ARTICLE_PRODUCT_PRODUCT1_idx` (`product_id` ASC),
   CONSTRAINT `fk_ARTICLE_PRODUCT_MARKETING_ARTICLE1`
     FOREIGN KEY (`article_id`)
     REFERENCES `mydb`.`MARKETING_ARTICLE` (`article_id`)
@@ -227,9 +209,10 @@ CREATE TABLE IF NOT EXISTS `mydb`.`PRODUCT_ORDER` (
   `order_id` INT NOT NULL,
   `product_id` INT NOT NULL,
   `number_product` INT NULL,
-  `price` DECIMAL(16,3) NULL,
-  INDEX `fk_PRODUCT_ORDER_ORDER1_idx` (`order_id` ASC) VISIBLE,
-  INDEX `fk_PRODUCT_ORDER_PRODUCT1_idx` (`product_id` ASC) VISIBLE,
+  `payment_mode` VARCHAR(45) NULL,
+  `option` VARCHAR(45) NULL,
+  INDEX `fk_PRODUCT_ORDER_ORDER1_idx` (`order_id` ASC),
+  INDEX `fk_PRODUCT_ORDER_PRODUCT1_idx` (`product_id` ASC),
   CONSTRAINT `fk_PRODUCT_ORDER_ORDER1`
     FOREIGN KEY (`order_id`)
     REFERENCES `mydb`.`ORDER` (`order_id`)
@@ -253,9 +236,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`HOT_PRODUCT` (
   `hot_display_id` INT NULL,
   `is_bigsale` TINYINT(1) NOT NULL,
   `bigsale_id` INT NULL,
-  INDEX `fk_BIGSALE_PRODUCT_PRODUCT1_idx` (`product_id` ASC) VISIBLE,
-  UNIQUE INDEX `hot_display_id_UNIQUE` (`hot_display_id` ASC) VISIBLE,
-  UNIQUE INDEX `bigsale_id_UNIQUE` (`bigsale_id` ASC) VISIBLE,
+  INDEX `fk_BIGSALE_PRODUCT_PRODUCT1_idx` (`product_id` ASC),
+  UNIQUE INDEX `hot_display_id_UNIQUE` (`hot_display_id` ASC),
+  UNIQUE INDEX `bigsale_id_UNIQUE` (`bigsale_id` ASC),
   CONSTRAINT `fk_BIGSALE_PRODUCT_PRODUCT1`
     FOREIGN KEY (`product_id`)
     REFERENCES `mydb`.`PRODUCT` (`product_id`)
